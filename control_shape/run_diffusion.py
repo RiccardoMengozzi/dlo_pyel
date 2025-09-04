41ee789b79a5c051906994f2a6b125b929962a97
Riccardo Mengozzi <riccardo.mengozzi3@studio.unibo.it>
2025-09-04 14:19:00 +0200

    fix

--- control_shape/run_diffusion.py
+++ control_shape/run_diffusion.py
@@ -70,58 +70,6 @@ DLO_0_N_TEST = np.array([[-0.24576193,  0.00006841],
         [ 0.23792662,  0.0001189 ],
         [ 0.24565348, -0.00008682]])
 
-# DLO_1_N_TEST =  np.array([[-0.24070267,  0.00292686],
-#         [-0.23132242, -0.00052248],
-#         [-0.22193218, -0.00398182],
-#         [-0.21255194, -0.00743116],
-#         [-0.20316169, -0.0108705 ],
-#         [-0.19375145, -0.01426983],
-#         [-0.18433122, -0.01762917],
-#         [-0.17489099, -0.02091851],
-#         [-0.16541076, -0.02410784],
-#         [-0.15589055, -0.02715717],
-#         [-0.14702029, -0.03084654],
-#         [-0.1373801 , -0.03350586],
-#         [-0.12681004, -0.03437512],
-#         [-0.11684998, -0.03519442],
-#         [-0.10684997, -0.03527371],
-#         [-0.09686001, -0.03472301],
-#         [-0.08692009, -0.03365231],
-#         [-0.0770302 , -0.03214161],
-#         [-0.06721033, -0.03027092],
-#         [-0.05744048, -0.02810023],
-#         [-0.04774065, -0.02569955],
-#         [-0.03807083, -0.02311887],
-#         [-0.02845102, -0.02039819],
-#         [-0.01886122, -0.01756751],
-#         [-0.00929143, -0.01466684],
-#         [ 0.00026837, -0.01172617],
-#         [ 0.00981816, -0.00875549],
-#         [ 0.01936795, -0.00578482],
-#         [ 0.02891774, -0.00282415],
-#         [ 0.03847753,  0.00010653],
-#         [ 0.04804733,  0.0030072 ],
-#         [ 0.05763713,  0.00584788],
-#         [ 0.06723693,  0.00864855],
-#         [ 0.07685674,  0.01137923],
-#         [ 0.08649655,  0.01404991],
-#         [ 0.09614637,  0.01666059],
-#         [ 0.10581619,  0.01920127],
-#         [ 0.11551601,  0.02167195],
-#         [ 0.12521584,  0.02408264],
-#         [ 0.13493568,  0.02642332],
-#         [ 0.14467552,  0.02870401],
-#         [ 0.15442536,  0.03092469],
-#         [ 0.16418521,  0.03309538],
-#         [ 0.17396506,  0.03520607],
-#         [ 0.18374492,  0.03727676],
-#         [ 0.19353477,  0.03930745],
-#         [ 0.20333463,  0.04130814],
-#         [ 0.21314449,  0.04327883],
-#         [ 0.22294435,  0.04522952],
-#         [ 0.23275422,  0.04717021],
-#         [ 0.24257408,  0.0491009 ]])
-
 DLO_1_N_TEST = np.array([
     [0.5000227,  0.23180239 ],
     [0.49989584, 0.22447005 ],
@@ -175,6 +123,60 @@ DLO_1_N_TEST = np.array([
     [0.49985456, -0.22016968],
     [0.5001113,  -0.2279048,],
 ])
+
+
+DLO_2_N_TEST = np.array([[-0.00383,  0.04268],
+ [ 0.00555,  0.04613],
+ [ 0.01494,  0.04959],
+ [ 0.02432,  0.05304],
+ [ 0.03371,  0.05648],
+ [ 0.04312,  0.05988],
+ [ 0.05254,  0.06324],
+ [ 0.06198,  0.06653],
+ [ 0.07146,  0.06972],
+ [ 0.08098,  0.07277],
+ [ 0.08985,  0.07646],
+ [ 0.09949,  0.07912],
+ [ 0.11006,  0.07999],
+ [ 0.12002,  0.08081],
+ [ 0.13002,  0.08089],
+ [ 0.14001,  0.08034],
+ [ 0.14995,  0.07927],
+ [ 0.15984,  0.07776],
+ [ 0.16966,  0.07589],
+ [ 0.17943,  0.07372],
+ [ 0.18913,  0.07132],
+ [ 0.1988 ,  0.06874],
+ [ 0.20842,  0.06602],
+ [ 0.21801,  0.06319],
+ [ 0.22758,  0.06029],
+ [ 0.23714,  0.05735],
+ [ 0.24669,  0.05438],
+ [ 0.25624,  0.05141],
+ [ 0.26579,  0.04845],
+ [ 0.27535,  0.04552],
+ [ 0.28492,  0.04262],
+ [ 0.29451,  0.03978],
+ [ 0.30411,  0.03698],
+ [ 0.31373,  0.03425],
+ [ 0.32337,  0.03158],
+ [ 0.33302,  0.02897],
+ [ 0.34269,  0.02643],
+ [ 0.35239,  0.02396],
+ [ 0.36209,  0.02155],
+ [ 0.37181,  0.01921],
+ [ 0.38155,  0.01693],
+ [ 0.3913 ,  0.01471],
+ [ 0.40106,  0.01254],
+ [ 0.41084,  0.01043],
+ [ 0.42062,  0.00836],
+ [ 0.43041,  0.00633],
+ [ 0.44021,  0.00433],
+ [ 0.45002,  0.00236],
+ [ 0.45982,  0.00041],
+ [ 0.46963, -0.00153],
+ [ 0.47945, -0.00346]])
+
 
 class DiffusionInference:
     def __init__(self, checkpoint_path, device="cpu"):
@@ -294,6 +296,85 @@ class DiffusionInference:
         return dlo_0, dlo_1, act
 
 
+def run_model_simulation(dlo_diff, dlo_params, init_shape, init_dir, target_shape, num_iterations=10):
+    """Run simulation for a single model and return all actions and shapes"""
+    dlo_0 = init_shape.copy()
+    dir_0 = init_dir.copy()
+    
+    all_actions = []
+    all_shapes = []
+
+    for i in tqdm(range(num_iterations), desc="performing actions"):
+
+        # run diffusion to get the predicted action
+        dlo_0_diff, dlo_1_diff, pred_action = dlo_diff.run(dlo_0, target_shape)
+
+        # convert the predicted action to the absolute frame for plotting
+        act_global_pos, _ = convert_action_horizon_to_absolute(dlo_0_diff, pred_action)
+
+        # apply the predicted action to the DLO model
+        action_idx = pred_action[0, 0]
+
+        list_shapes, list_directors = [], []
+        for h in range(pred_action.shape[0]):
+            dlo = DloModel(dlo_params, position=dlo_0, directors=dir_0)
+            dlo.build_model(action=pred_action[h])
+            dict_out = dlo.run_simulation(progress_bar=False)
+
+            list_shapes.append(dict_out["final_shape"])
+            list_directors.append(dict_out["final_directors"])
+
+            # update dlo_0 and dir_0 for the next iteration
+            dlo_0 = list_shapes[-1]
+            dir_0 = list_directors[-1]
+
+        list_shapes_ok = np.array([shape.T for shape in list_shapes])
+        all_actions.append(pred_action)
+        all_shapes.append(list_shapes_ok)
+
+    all_actions = np.array(all_actions)
+    all_shapes = np.array(all_shapes)
+    
+    return all_actions, all_shapes
+
+
+def plot_single_model(ax, iteration, all_actions, all_shapes, init_shape_ok, target_shape_ok, model_name):
+    """Plot results for a single model on given axis"""
+    ax.clear()
+    
+    # Get current action and shapes
+    action = all_actions[iteration]
+    simulation_shapes = all_shapes[iteration]
+    
+    # Plot initial and target shapes
+    ax.plot(init_shape_ok[:, 0], init_shape_ok[:, 1], label="Initial Shape", marker="o", zorder=5)
+    ax.plot(target_shape_ok[:, 0], target_shape_ok[:, 1], label="Target Shape", marker="x", zorder=4)
+    
+    # Plot all shapes from the current simulation run
+    for j, shape in enumerate(simulation_shapes):
+        ax.plot(shape[:, 0], shape[:, 1], color="black", linestyle="--", alpha=0.5, zorder=1)
+    
+    # Highlight the first and final shapes of the current run
+    ax.plot(simulation_shapes[0][:, 0], simulation_shapes[0][:, 1], label="First Shape", marker="o", color="blue", zorder=2)
+    ax.plot(simulation_shapes[-1][:, 0], simulation_shapes[-1][:, 1], label="Final Shape", marker="o", color="red", zorder=3)
+
+    # Get the predicted action for this iteration
+    pred_action_iter = action
+    act_global_pos, _ = convert_action_horizon_to_absolute(simulation_shapes[0][:, :2], pred_action_iter)
+
+    color_vals = np.linspace(0, 1, act_global_pos.shape[0])
+    scatter = ax.scatter(
+        act_global_pos[:, 0], act_global_pos[:, 1], c=color_vals, cmap="inferno", s=50, alpha=0.8, zorder=6
+    )
+    
+    ax.set_title(f"{model_name} - Iteration {iteration+1}")
+    ax.set_xlabel("X Position")
+    ax.set_ylabel("Y Position")
+    ax.legend()
+    ax.grid()
+    ax.axis("equal")
+
+
 if __name__ == "__main__":
 
     CONFIG = {
@@ -318,54 +399,38 @@ if __name__ == "__main__":
         action_velocity=CONFIG["action_velocity"],
     )
 
-
     MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
     DATA_PATH = os.path.join(MAIN_DIR, "DATA/train")
-    CHECKPOINT_PATH = os.path.join(MAIN_DIR, "checkpoints/diffusion_super-brook-8_best.pt")
-
-
-    dlo_diff = DiffusionInference(CHECKPOINT_PATH, device="cuda")
+    
+    # Define paths for both models
+    CHECKPOINT_PATH_1 = os.path.join(MAIN_DIR, "checkpoints/diffusion_super-brook-8_best.pt")
+    CHECKPOINT_PATH_2 = os.path.join(MAIN_DIR, "checkpoints/diffusion_sage-pine-63_best.pt")  # Replace with your second model path
+    
+    # Load both models
+    print("Loading Model 1...")
+    dlo_diff_1 = DiffusionInference(CHECKPOINT_PATH_1, device="cuda")
+    
+    print("Loading Model 2...")
+    dlo_diff_2 = DiffusionInference(CHECKPOINT_PATH_2, device="cuda")
 
     ################################
 
-    # # ##########
-    # # random action to initialize the DLO model
+    # Initialize DLO model and get initial conditions
     action = [0, 0.0, 0.0, 0.0]
-
     dlo = DloModel(dlo_params)
     dlo.build_model(action=action)
     dict_out = dlo.run_simulation()
     init_shape = dict_out["final_shape"]
     init_dir = dict_out["final_directors"]
 
-
-    # ##########
-    # # new model with the initial shape and directors -> get target observation
-    # gt_action = [10, 0.0, 0.05, 0.0]
-
-    # dlo = DloModel(dlo_params, position=init_shape, directors=init_dir)
-    # dlo.build_model(action=gt_action)
-    # dict_out = dlo.run_simulation()
-    # target_shape = dict_out["final_shape"]
-    # target_dir = dict_out["final_directors"]
-
-
-
-    ######### TEST #########
-
-    # init_shape = DLO_0_N_TEST
-    # init_dir, init_shape = create_directors_from_positions(init_shape)
-    # init_shape = init_shape.T
-    # init_dir = np.array(init_dir)[0:-1]
-    # init_dir = np.moveaxis(init_dir, 0, -1)
-
+    # Set up target shape
     R = [[0.0, 1.0],
          [-1.0, 0.0]]
     target_shape = DLO_1_N_TEST @ R 
     target_dir, target_shape = create_directors_from_positions(target_shape)
     target_shape = target_shape.T
 
-    # move centers to be in same position
+    # Move centers to be in same position
     init_shape_center = np.mean(init_shape, axis=1)
     target_shape_center = np.mean(target_shape, axis=1)
     centers_distance = target_shape_center - init_shape_center
@@ -374,114 +439,60 @@ if __name__ == "__main__":
     target_dir = np.array(target_dir)[0:-1]
     target_dir = np.moveaxis(target_dir, 0, -1)
 
-
-    # import glob, pickle
-    # test_dataset_path = os.path.join(MAIN_DIR, "TEST")
-    # data_files = glob.glob(os.path.join(test_dataset_path, "*.pkl"))
-    # print("Found {} files in dataset {}".format(len(data_files), test_dataset_path))
-    # data_files.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split("_")[0]))
-    # init_data = pickle.load(open(data_files[0], "rb"))
-    # final_data = pickle.load(open(data_files[9], "rb"))
-
-    # init_shape = init_data["init_shape"]
-    # init_dir = init_data["init_directors"]
-
-    # target_shape = final_data["final_shape"]
-    # target_dir = final_data["final_directors"]
+    # Alternative: Load from dataset
+    import glob, pickle
+    test_dataset_path = "/home/lar/Riccardo/dlo_diffusion/DATA_500k/val"
+    data_files = glob.glob(os.path.join(test_dataset_path, "*.pkl"))
+    print("Found {} files in dataset {}".format(len(data_files), test_dataset_path))
+    data_files.sort(key=lambda x: int(os.path.splitext(os.path.basename(x))[0].split("_")[0]))
+    init_data = pickle.load(open(data_files[0], "rb"))
+    final_data = pickle.load(open(data_files[0], "rb"))
+    #init_shape = init_data["init_shape"]
+    print("init_data", repr(init_shape))
+    #init_dir = init_data["init_directors"]
+    target_shape = final_data["final_shape"]
+    print("init_data", repr(target_shape))
+    target_dir = final_data["final_directors"]
 
     ########################################
 
+    # Run simulations for both models
+    print("Running simulation for Model 1...")
+    all_actions_1, all_shapes_1 = run_model_simulation(dlo_diff_1, dlo_params, init_shape, init_dir, target_shape, num_iterations=10)
+    
+    print("Running simulation for Model 2...")
+    all_actions_2, all_shapes_2 = run_model_simulation(dlo_diff_2, dlo_params, init_shape, init_dir, target_shape, num_iterations=10)
 
-    dlo_0 = init_shape.copy()
-    dir_0 = init_dir.copy()
-    dlo_1 = target_shape.copy()
-    dir_1 = target_dir.copy()
-
-
-    all_actions = []
-    all_shapes = []
-
-    for i in tqdm(range(100), desc="performing actions"):
-
-        # run diffusion to get the predicted action
-        dlo_0_diff, dlo_1_diff, pred_action = dlo_diff.run(dlo_0, target_shape)
-
-        # convert the predicted action to the absolute frame for plotting
-        act_global_pos, _ = convert_action_horizon_to_absolute(dlo_0_diff, pred_action)
-
-        # apply the predicted action to the DLO model
-        action_idx = pred_action[0, 0]
-
-        list_shapes, list_directors = [], []
-        for h in range(pred_action.shape[0]):
-            dlo = DloModel(dlo_params, position=dlo_0, directors=dir_0)
-            dlo.build_model(action=pred_action[h])
-            dict_out = dlo.run_simulation(progress_bar=False)
-
-            list_shapes.append(dict_out["final_shape"])
-            list_directors.append(dict_out["final_directors"])
-
-            # update dlo_0 and dir_0 for the next iteration
-            dlo_0 = list_shapes[-1]
-            dir_0 = list_directors[-1]
-
-
-
-        list_shapes_ok = np.array([shape.T for shape in list_shapes])
-        all_actions.append(pred_action)
-        all_shapes.append(list_shapes_ok)
-
-
-    all_actions = np.array(all_actions)
-    all_shapes = np.array(all_shapes)
-
-    # Create the figure and axis
-    fig, ax = plt.subplots(figsize=(10, 8))
-    plt.subplots_adjust(bottom=0.2)  # Make room for the slider
-
+    # Prepare data for plotting
     init_shape_ok = init_shape.T
     target_shape_ok = target_shape.T
 
+    # Create the figure with two subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 8))
+    plt.subplots_adjust(bottom=0.15)  # Make room for the slider
+
     # Create slider
     ax_slider = plt.axes([0.2, 0.05, 0.6, 0.03])
-    slider = Slider(ax_slider, 'Iteration', 0, len(all_actions)-1, valinit=0, valstep=1, valfmt='%d')
+    max_iterations = max(len(all_actions_1), len(all_actions_2)) - 1
+    slider = Slider(ax_slider, 'Iteration', 0, max_iterations, valinit=0, valstep=1, valfmt='%d')
 
     def update_plot(iteration):
-        """Update the plot based on the slider value"""
+        """Update both plots based on the slider value"""
         iteration = int(iteration)
-        ax.clear()
         
-        # Get current action and shapes
-        action = all_actions[iteration]
-        simulation_shapes = all_shapes[iteration]
+        # Plot Model 1 (only if iteration is within range)
+        if iteration < len(all_actions_1):
+            plot_single_model(ax1, iteration, all_actions_1, all_shapes_1, init_shape_ok, target_shape_ok, "Model 1")
+        else:
+            ax1.clear()
+            ax1.set_title(f"Model 1 - No data for iteration {iteration+1}")
         
-        # Plot initial and target shapes
-        ax.plot(init_shape_ok[:, 0], init_shape_ok[:, 1], label="Initial Shape", marker="o", zorder=5)
-        ax.plot(target_shape_ok[:, 0], target_shape_ok[:, 1], label="Target Shape", marker="x", zorder=4)
-        
-        # Plot all shapes from the current simulation run
-        for j, shape in enumerate(simulation_shapes):
-            ax.plot(shape[:, 0], shape[:, 1], color="black", linestyle="--", alpha=0.5, zorder=1)
-        
-        # Highlight the first and final shapes of the current run
-        ax.plot(simulation_shapes[0][:, 0], simulation_shapes[0][:, 1], label="First Shape", marker="o", color="blue", zorder=2)
-        ax.plot(simulation_shapes[-1][:, 0], simulation_shapes[-1][:, 1], label="Final Shape", marker="o", color="red", zorder=3)
-
-        # Get the predicted action for this iteration
-        pred_action_iter = action
-        act_global_pos, _ = convert_action_horizon_to_absolute(simulation_shapes[0][:, :2], pred_action_iter)
-
-        color_vals = np.linspace(0, 1, act_global_pos.shape[0])
-        scatter = ax.scatter(
-            act_global_pos[:, 0], act_global_pos[:, 1], c=color_vals, cmap="inferno", s=50, alpha=0.8, zorder=6
-        )
-        
-        ax.set_title(f"Diffusion DLO Simulation - Iteration {iteration+1}")
-        ax.set_xlabel("X Position")
-        ax.set_ylabel("Y Position")
-        ax.legend()
-        ax.grid()
-        ax.axis("equal")
+        # Plot Model 2 (only if iteration is within range)
+        if iteration < len(all_actions_2):
+            plot_single_model(ax2, iteration, all_actions_2, all_shapes_2, init_shape_ok, target_shape_ok, "Model 2")
+        else:
+            ax2.clear()
+            ax2.set_title(f"Model 2 - No data for iteration {iteration+1}")
         
         plt.tight_layout()
         fig.canvas.draw()
--- from_pkl_to_zarr.py
+++ from_pkl_to_zarr.py
@@ -4,24 +4,28 @@ import zarr
 import numpy as np
 from tqdm import tqdm
 import datetime
+from multiprocessing import Pool, cpu_count
+
 
 # -----------------------------
 # CONFIGURATION
 # -----------------------------
-PKL_FOLDER = "/home/mengo/research/dlo_pyel/dataset_20250901_131816"   # cartella sorgente con i file .pkl
+PKL_FOLDER = "/home/lar/Riccardo/dlo_pyel/dataset_20250901_132645"
 SAVE_PATH = f"dataset_zarr_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}/"
 
 os.makedirs(SAVE_PATH, exist_ok=True)
 print(f"Converting PKL dataset from {PKL_FOLDER} to Zarr dataset at {SAVE_PATH}")
 
+
 # -----------------------------
 # CONVERSION FUNCTION
 # -----------------------------
-def convert_pkl_to_zarr(pkl_file, zarr_file):
-    with open(pkl_file, "rb") as f:
+def convert_pkl_to_zarr(fname):
+    pkl_path, zarr_path = fname
+    with open(pkl_path, "rb") as f:
         data = pickle.load(f)
 
-    root = zarr.open_group(zarr_file, mode="w")
+    root = zarr.open_group(zarr_path, mode="w")
 
     for key, value in data.items():
         if isinstance(value, np.ndarray):
@@ -29,27 +33,29 @@ def convert_pkl_to_zarr(pkl_file, zarr_file):
                 key,
                 data=value,
                 chunks=True,
-                compressor=zarr.get_codec({'id': 'zlib'})  # compressione
+                compressor=False  
             )
         elif isinstance(value, (int, float, str, np.int32, np.float32)):
             root.attrs[key] = value
         else:
-            # fallback: salvo come stringa se non è numpy/scalare
             root.attrs[key] = str(value)
 
 
 # -----------------------------
-# MAIN LOOP
+# MAIN LOOP (PARALLEL)
 # -----------------------------
 def main():
     pkl_files = sorted([f for f in os.listdir(PKL_FOLDER) if f.endswith(".pkl")])
+    tasks = [
+        (
+            os.path.join(PKL_FOLDER, fname),
+            os.path.join(SAVE_PATH, os.path.splitext(fname)[0] + ".zarr"),
+        )
+        for fname in pkl_files
+    ]
 
-    for fname in tqdm(pkl_files, desc="Converting"):
-        pkl_path = os.path.join(PKL_FOLDER, fname)
-        base_name = os.path.splitext(fname)[0] + ".zarr"
-        zarr_path = os.path.join(SAVE_PATH, base_name)
-
-        convert_pkl_to_zarr(pkl_path, zarr_path)
+    with Pool(processes=32) as pool:
+        list(tqdm(pool.imap_unordered(convert_pkl_to_zarr, tasks), total=len(tasks)))
 
     print("Conversion completed.")
 
